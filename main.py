b99711c4798c1c7306c6d92f2c7881438d742084
yymin1022 <yymin1022@gmail.com>
2022-01-26 15:40:00 +0900

    Update main.py : Add Sending Logic

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
 import discord
 
-tokenFile = open("/home/server/Wa_Bot_Discord/TOKEN_FILE", 'r')
-TOKEN = tokenFile.read().rstrip('\n')
+tokenFile = open("/home/server/Wa_Bot_Discord/TOKEN_FILE", "r")
+TOKEN = tokenFile.read().rstrip("\n")
 tokenFile.close()
 
 client = discord.Client()
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print("We have logged in as {0.user}".format(client))
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+    if message.content.startswith("$hello"):
+        await message.channel.send("Hello!")
+    
+    sendWaMessage(message.content, message.channel, message.author)
+
+def sendWaMessage(messageContent, messageChannel, messageAuthor):
+    requestData = dict([("msg", messageContent), ("room", messageChannel), ("sender", messageAuthor)])
+    resultData = requests.post("https://wa-api.defcon.or.kr/getMessage", json=requestData).json()
+
+    resultMessage = resultData["DATA"]["msg"]
+
+    if resultData["RESULT"]["RESULT_CODE"] == 0:
+        if resultMessage.find("\\m") > 0:
+            resultMessageList = resultMessage.split("\\m")
+
+            for resultMessageItem in resultMessageList:
+                await messageChannel.send(resultMessageItem)
+        else:
+            resultMessage = resultMessage.replace("\\n", "\n")
+            await messageChannel.send(resultMessage)
 
 client.run(TOKEN)
